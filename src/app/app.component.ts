7e66327ffbe10de73d82a2f2d0266c51fd0fff0f
Gabriel Veloso Pinheiro <tr26660es@trf1.gov.br>
2025-03-26 15:38:52 -0300

    develop: Finished Pokédex Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CardComponent} from './components/card/card.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { PokemonService } from '../../services/pokemon.service';
+import { PokemonData } from '../../models/pokemonData';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  pokemon: PokemonData;
 
+  constructor(private service: PokemonService) {
+    this.pokemon = {
+      id: 0,
+      name: '',
+      sprites: {
+        front_default: ''
+      },
+      types: []
+    };
+  }
+
+  ngOnInit(): void {
+    this.getPokemon('pikachu');
+  }
+
+  getPokemon(searchName: string) {
+    this.service.getPokemon(searchName).subscribe(
+      {
+        next: (response) => {
+          this.pokemon = {
+            id: response.id,
+            name: response.name,
+            sprites: response.sprites,
+            types: response.types
+          };
+        }
+      }
+    );
+  }
 }
